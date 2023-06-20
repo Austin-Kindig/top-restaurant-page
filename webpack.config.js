f824b7e5723a9dca0001278d80651b07d86f8d4c
Austin-Kindig <kindig.austin.ak@gmail.com>
2023-06-20 14:45:55 -0400

    modify webpack for distibution folder recreation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,15 +8,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-     rules: [
-       {
-         test: /\.css$/i,
-         use: ['style-loader', 'css-loader'],
-       },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-     ],
-   },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
 };
